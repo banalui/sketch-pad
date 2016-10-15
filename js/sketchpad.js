e17d8968b4e2f4a8acbc51d10123617228a6bab0
Behdad <behdad.analui@gmail.com>
2016-10-14 17:04:59 -0700

    Added all 3 buttons

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,27 +1,68 @@
 $(document).ready(function(){
 
 	// ********* Create 16x16 Grid *********
-	for(i = 1 ; i <= 16 ; i++){
-		for(j = 1 ; j <= 16 ; j++){
-			$("body").append("<div>Hi" + i + " " + j + "</div>");
-		}
-		$("body").append("<br>");
-	}
-
-	// ********* Set the default properties *****
-
-	$("div").css("display", "inline-block");
-	$("div").height(70);
-	$("div").width(70);
-	$("div").css("background-color", "orange");
-	$("div").css("margin", "5px");
-
-	// ********* Change color when hover ********
-
-	$("div").hover(function(){
+	createGrid(16);
+	$(".grid").hover(function(){
 		$(this).css("background-color", "blue");
 	});
 });
+
+function redoSketchPad(){
+	var numberOfGridPerSide = prompt("How many boxes you need per side? Please enter a number between 1 and 128");
+	createGrid(numberOfGridPerSide);
+	$(".grid").hover(function(){
+		$(this).css("background-color", "blue");
+	});
+}
+
+function randomColor(){
+	var numberOfGridPerSide = prompt("How many boxes you need per side? Please enter a number between 1 and 128");
+	createGrid(numberOfGridPerSide);
+	$(".grid").hover(function(){
+		var randomColorR = Math.floor((Math.random() * 1000000) % 256);
+		var randomColorG = Math.floor((Math.random() * 1000000) % 256);
+		var randomColorB = Math.floor((Math.random() * 1000000) % 256);
+		var rgbString = "rgb(" + randomColorR + ", " + randomColorG + ", " + randomColorB + ")";
+		$(this).css("background-color", rgbString);
+	});
+}
+
+function trailColor(){
+	var numberOfGridPerSide = prompt("How many boxes you need per side? Please enter a number between 1 and 128");
+	createGrid(numberOfGridPerSide);
+	var randomColorR = 255;
+	var randomColorG = 255;
+	var randomColorB = 255;
+	$(".grid").hover(function(){
+		randomColorR = Math.floor(randomColorR - 25);
+		randomColorG = Math.floor(randomColorG - 25);
+		randomColorB = Math.floor(randomColorB - 25);
+		if(randomColorR <= 0){
+			randomColorR = 255;
+			randomColorG = 255;
+			randomColorB = 255;
+		}
+		var rgbString = "rgb(" + randomColorR + ", " + randomColorG + ", " + randomColorB + ")";
+		$(this).css("background-color", rgbString);
+	});
+}
+
+function createGrid(numberOfElementsPerSide){
+	$(".row").remove();
+	for(i = 1 ; i <= numberOfElementsPerSide; i++){
+		$(".container").append('<div class="row"></div>');
+	}
+	var gridSize = Math.floor(1100 / numberOfElementsPerSide);
+	var newPixelMax = gridSize * numberOfElementsPerSide;
+	$(".container").height(newPixelMax);
+	$(".container").width(newPixelMax);
+	$(".row").height(gridSize);
+	for(j = 1 ; j <= numberOfElementsPerSide; j++){
+		$(".container").children().append('<div class="grid"></div>');
+	}
+	$(".grid").height(gridSize);
+	$(".grid").width(gridSize);
+}
 
 
 
